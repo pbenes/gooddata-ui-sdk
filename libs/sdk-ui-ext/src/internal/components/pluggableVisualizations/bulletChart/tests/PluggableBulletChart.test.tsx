77e66cefd63a8614fb937286c2aa89892a908b8d
Petr Benes <petr.benes@gooddata.com>
2020-09-11 13:16:57 +0200

    Bullet chart test, some helpers: move anywhere?

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/bulletChart/tests/PluggableBulletChart.test.tsx
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/bulletChart/tests/PluggableBulletChart.test.tsx
@@ -2,10 +2,37 @@
 import noop from "lodash/noop";
 import { PluggableBulletChart } from "../PluggableBulletChart";
 import * as referencePointMocks from "../../../../tests/mocks/referencePointMocks";
-import { IBucketOfFun, IFilters, IReferencePoint, IVisConstruct } from "../../../../interfaces/Visualization";
+import {
+    IBucketOfFun,
+    IFilters,
+    IReferencePoint,
+    IVisConstruct,
+    IImplicitDrillDown,
+} from "../../../../interfaces/Visualization";
 import { DEFAULT_BULLET_CHART_CONFIG } from "../../../../constants/uiConfig";
-import { OverTimeComparisonTypes, BucketNames } from "@gooddata/sdk-ui";
+import {
+    OverTimeComparisonTypes,
+    BucketNames,
+    IDrillEventIntersectionElement,
+    DrillEventIntersectionElementHeader,
+    IDrillEvent,
+    VisType,
+} from "@gooddata/sdk-ui";
 import { dummyBackend } from "@gooddata/sdk-backend-mockingbird";
+import {
+    newAttribute,
+    IAttribute,
+    IInsight,
+    IInsightDefinition,
+    newInsightDefinition,
+    newBucket,
+    newNegativeAttributeFilter,
+    newPositiveAttributeFilter,
+    uriRef,
+} from "@gooddata/sdk-model";
+import { IMeasureDescriptor } from "@gooddata/sdk-backend-spi";
+import { IDrillIntersectionAttributeItem } from "@gooddata/sdk-ui";
+import { Department, Region, Won } from "@gooddata/reference-workspace/dist/ldm/full";
 
 const defaultProps: IVisConstruct = {
     backend: dummyBackend(),
@@ -484,8 +511,147 @@ describe("PluggableBulletChart", () => {
     });
 
     describe("Drill Down", () => {
+        function createMeasureDescriptor(name: string, localIdentifier: string): IMeasureDescriptor {
+            return {
+                measureHeaderItem: {
+                    localIdentifier,
+                    name,
+                    format: "###.#",
+                },
+            };
+        }
+
+        function createAttributeDescriptor(
+            name: string,
+            localIdentifier: string,
+        ): IDrillIntersectionAttributeItem {
+            return {
+                attributeHeaderItem: {
+                    uri: `/gdc/md/${name}-element`,
+                    name: `${name}-element-name`,
+                },
+                attributeHeader: {
+                    uri: `/gdc/md/obj/${name}`,
+                    identifier: `id-${name}`,
+                    localIdentifier,
+                    name,
+                    formOf: {
+                        uri: `/gdc/md/obj/formof-${name}`,
+                        identifier: `formof-id-${name}`,
+                        name: `formof-${name}`,
+                    },
+                },
+            };
+        }
+
+        function wrapHeader(header: DrillEventIntersectionElementHeader): IDrillEventIntersectionElement {
+            return { header };
+        }
+
+        function createDrillDefinition(fromAttribute: IAttribute, targetUri: string): IImplicitDrillDown {
+            return {
+                implicitDrillDown: {
+                    from: {
+                        drillFromAttribute: { localIdentifier: fromAttribute.attribute.localIdentifier },
+                    },
+                    target: {
+                        drillToAttribute: {
+                            attributeDisplayForm: uriRef(targetUri),
+                        },
+                    },
+                },
+            };
+        }
+
+        function wrapUriIdentifier(
+            insightDefinition: IInsightDefinition,
+            uri: string,
+            identifier: string,
+        ): IInsight {
+            return {
+                ...insightDefinition,
+                insight: {
+                    ...insightDefinition.insight,
+                    identifier,
+                    uri,
+                },
+            };
+        }
+
+        function createDrillEvent(
+            type: VisType,
+            intersection: IDrillEventIntersectionElement[],
+        ): IDrillEvent {
+            return {
+                dataView: null,
+                drillContext: {
+                    type,
+                    intersection,
+                    element: null,
+                },
+            };
+        }
+
+        const measure = wrapHeader(createMeasureDescriptor("m", "m_acugFHNJgsBy"));
+        const attribute1 = wrapHeader(createAttributeDescriptor("a1", "a1"));
+        const attribute2 = wrapHeader(createAttributeDescriptor("a2", Region.attribute.localIdentifier));
+        const attribute3 = wrapHeader(createAttributeDescriptor("a3", "a3"));
+        const targetUri = "target-uri";
+
+        const intersection = [measure, attribute1, attribute2, attribute3];
+
         it("should replace the drill down attribute and add intersection filters", () => {
-            fail("not implemented");
+            const bulletChart = createComponent();
+
+            const drillDefinition = createDrillDefinition(Region, targetUri);
+
+            const sourceInsightDefinition: IInsightDefinition = newInsightDefinition(
+                "visualizationClass-url",
+                (b) => {
+                    return b
+                        .title("sourceInsight")
+                        .buckets([
+                            newBucket("measure", Won),
+                            newBucket("view", Region, Department),
+                            newBucket("stack", Department),
+                        ])
+                        .filters([newNegativeAttributeFilter(Department, [])]);
+                },
+            );
+            const sourceInsight = wrapUriIdentifier(sourceInsightDefinition, "first", "first");
+            const replacedAttribute = newAttribute(uriRef(targetUri), (b) =>
+                b.localId(Region.attribute.localIdentifier),
+            );
+
+            const expectedInsightDefinition: IInsightDefinition = newInsightDefinition(
+                "visualizationClass-url",
+                (b) => {
+                    return b
+                        .title("sourceInsight")
+                        .buckets([
+                            newBucket("measure", Won),
+                            newBucket("view", replacedAttribute, Department),
+                            newBucket("stack", Department),
+                        ])
+                        .filters([
+                            newNegativeAttributeFilter(Department, []),
+                            newPositiveAttributeFilter(newAttribute(uriRef("/gdc/md/obj/a2")), {
+                                uris: ["/gdc/md/a2-element"],
+                            }),
+                            newPositiveAttributeFilter(newAttribute(uriRef("/gdc/md/obj/a3")), {
+                                uris: ["/gdc/md/a3-element"],
+                            }),
+                        ]);
+                },
+            );
+            const expectedInsight = wrapUriIdentifier(expectedInsightDefinition, "first", "first");
+
+            const result: IInsight = bulletChart.modifyInsightForDrillDown(sourceInsight, {
+                drillDefinition,
+                event: createDrillEvent("bullet", intersection),
+            });
+
+            expect(result).toEqual(expectedInsight);
         });
     });
 });
