937466af955e95b839523b6ba59935708c3f25b3
ivanmjartan <ivan.mjartan@gooddata.com>
2021-04-15 09:00:30 +0200

    Fix legend ico

--- libs/sdk-ui-vis-commons/src/legend/Legend.tsx
+++ libs/sdk-ui-vis-commons/src/legend/Legend.tsx
@@ -10,7 +10,7 @@ import { StaticLegend, IStaticLegendProps } from "./StaticLegend";
 import { HeatmapLegend } from "./HeatmapLegend";
 import { IntlWrapper, IntlTranslationsProvider, ITranslationsComponentProps } from "@gooddata/sdk-ui";
 import { ItemBorderRadiusPredicate } from "./types";
-import { PopUpLegend } from "./PopUpLegend/PopUpLegend";
+import { PopUpLegend } from "./PopUpLegend/PopupLegend";
 
 /**
  * @internal
--- libs/sdk-ui-vis-commons/src/legend/LegendList.tsx
+++ libs/sdk-ui-vis-commons/src/legend/LegendList.tsx
@@ -6,7 +6,7 @@ import { LEGEND_AXIS_INDICATOR, LEGEND_SEPARATOR } from "./helpers";
 import { ItemBorderRadiusPredicate } from "./types";
 
 export interface ILegendListProps {
-    series: any;
+    series: any; // TODO Add typings
     enableBorderRadius?: boolean | ItemBorderRadiusPredicate;
     width?: number;
     onItemClick: (item: any) => void;
