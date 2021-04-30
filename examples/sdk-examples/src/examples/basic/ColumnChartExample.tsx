0add28809fd653bc4f85103c7c1b97727020e4ff
Petr Benes <petr.benes@gooddata.com>
2021-04-30 08:54:16 +0200

    example

--- examples/sdk-examples/src/examples/basic/ColumnChartExample.tsx
+++ examples/sdk-examples/src/examples/basic/ColumnChartExample.tsx
@@ -9,11 +9,7 @@ const style = { height: 300 };
 export const ColumnChartExample: React.FC = () => {
     return (
         <div style={style} className="s-column-chart">
-            <ColumnChart
-                measures={[LdmExt.TotalSales1]}
-                viewBy={LdmExt.LocationState}
-                stackBy={LdmExt.monthDate}
-            />
+            <ColumnChart measures={[LdmExt.TotalSales1]} viewBy={LdmExt.monthDate} />
         </div>
     );
 };
