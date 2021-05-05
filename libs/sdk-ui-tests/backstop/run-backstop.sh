52d137f075ede7d2898f3f61515a6863731361ca
ivanmjartan <ivan.mjartan@gooddata.com>
2021-05-05 09:38:38 +0200

    Add screens

--- libs/sdk-ui-tests/backstop/run-backstop.sh
+++ libs/sdk-ui-tests/backstop/run-backstop.sh
@@ -59,8 +59,6 @@ docker network create "${BACKSTOP_NET}" || { echo "Network creation failed" && e
 
     echo "Cleaning up the backstop output directory."
 
-    rm -rf "${BACKSTOP_DIR}/output"
-
     echo "Starting BackstopJS in dir ${BACKSTOP_DIR} with params: $@"
 
     {
