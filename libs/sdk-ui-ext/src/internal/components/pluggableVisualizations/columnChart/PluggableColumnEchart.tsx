207de51c2e5417d82b9dab55f729611451d1986a
Petr Benes <petr.benes@gooddata.com>
2022-11-10 11:08:09 +0100

    add lib

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/columnChart/PluggableColumnEchart.tsx
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/columnChart/PluggableColumnEchart.tsx
@@ -10,6 +10,7 @@ import { canSortStackTotalValue } from "../barChart/sortHelpers";
 import { ISortConfig, newAvailableSortsGroup } from "../../../interfaces/SortConfig";
 import { getCustomSortDisabledExplanation } from "../../../utils/sort";
 import { PluggableBaseChart } from "../baseChart/PluggableBaseChart";
+import * as echarts from "echarts";
 
 /**
  * PluggableColumnChart
@@ -63,7 +64,32 @@ export class PluggableColumnEchart extends PluggableBaseChart {
 
     protected renderVisualization(options: any, insight: any, executionFactory: any): void {
         console.log(options, insight, executionFactory);
-        this.getElement().innerHTML = "fooooo";
+
+        var chartDom = this.getElement();
+        var myChart = echarts.init(chartDom);
+        var option;
+
+        option = {
+            xAxis: {
+                type: "category",
+                data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            },
+            yAxis: {
+                type: "value",
+            },
+            series: [
+                {
+                    data: [120, 200, 150, 80, 70, 110, 130],
+                    type: "bar",
+                    showBackground: false,
+                    backgroundStyle: {
+                        color: "rgba(180, 180, 180, 0.2)",
+                    },
+                },
+            ],
+        };
+
+        option && myChart.setOption(option);
     }
 
     protected getDefaultAndAvailableSort(
